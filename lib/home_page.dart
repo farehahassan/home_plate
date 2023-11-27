830793963452c53f599369998a0154841585deb7
Fareha <hfariha104@gmail.com>
2023-11-27 23:43:35 +0500

    inital commit

---
+++ lib/home_page.dart
@@ -0,0 +1,136 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first, unused_import
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:home_plate/home_screen.dart';
+
+import 'package:home_plate/onboardlist.dart';
+// / import 'package:onboarding/onboardlist.dart';
+
+// import 'package:home_plate/onboardlist.dart';
+
+// import 'home_screen.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({
+    Key? key,
+    // required this.currentIndex,
+  }) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(actions: [
+        Text(
+          "Skip",
+          style: TextStyle(fontSize: 18.sp, fontWeight: FontWeight.w400),
+        ),
+        SizedBox(
+          width: 20.w,
+        )
+      ]),
+      body: PageView.builder(
+        onPageChanged: (index) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
+        itemCount: pages.length,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: 20.sp,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  pages[index].image,
+                  width: 250.w,
+                ),
+                Text(
+                  pages[index].title,
+                  style: GoogleFonts.aoboshiOne(
+                      fontSize: 30.sp,
+                      fontWeight: FontWeight.w400,
+                      letterSpacing: -0.17.sp,
+                      color: Colors.black),
+                ),
+                SizedBox(
+                  height: 8.h,
+                ),
+                Text(
+                  pages[index].discription,
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.roboto(
+                      color: Color(0xffAAAAAA),
+                      fontSize: 20.sp,
+                      fontWeight: FontWeight.w400),
+                ),
+                SizedBox(
+                  height: 19.h,
+                ),
+                Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: List.generate(
+                      3,
+                      (index) => Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 10.sp),
+                        child: Container(
+                          height: currentIndex == index ? 13.h : 10.h,
+                          width: currentIndex == index ? 13.w : 10.w,
+                          decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: currentIndex == index
+                                  ? Color(0xffE29A4F)
+                                  : Color(0xffE7E7E7)),
+                        ),
+                      ),
+                    )),
+                SizedBox(
+                  height: 28.h,
+                ),
+                InkWell(
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (_) => HomeScreen()));
+                  },
+                  child: Container(
+                    width: 347.w,
+                    height: 55.h,
+                    decoration: BoxDecoration(
+                      color: Color(0xffE29A4F),
+                      borderRadius: BorderRadius.circular(20.r),
+                    ),
+                    child: Center(
+                        child: Text(
+                      currentIndex == pages.length - 1 ? "Start" : "Next",
+                      style: GoogleFonts.roboto(
+                          fontSize: 20.sp,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w400),
+                    )),
+                  ),
+                ),
+                SizedBox(
+                  height: 50.h,
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
---
+++ lib/onboardlist.dart
@@ -0,0 +1,23 @@
+class OnboardingList {
+  String image;
+  String title;
+  String discription;
+
+  OnboardingList(
+      {required this.image, required this.title, required this.discription});
+}
+
+List<OnboardingList> pages = [
+  OnboardingList(
+      title: "Healthy Food",
+      image: "assets/images/onboard1.png",
+      discription: "Eat today live another\n memorable day"),
+  OnboardingList(
+      title: "Tasty Food ",
+      image: "assets/images/onboard2.png",
+      discription: "Let's eat some diet food\n while steak to cock"),
+  OnboardingList(
+      title: "Let's eat",
+      image: "assets/images/onboard3.png",
+      discription: "Food is really and truly the\n most effective medicine"),
+];
